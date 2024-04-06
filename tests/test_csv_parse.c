f3cb70a3118c451288b4bc5ef0379c53e72b5e0d
Andrei <andreisva2023@gmail.com>
2024-04-06 16:15:14 -0700

    add new tests
    tests fail again now because the CSV parser doesn't correctly handle unicode

--- tests/test_csv_parse.c
+++ tests/test_csv_parse.c
@@ -9,13 +9,24 @@
 
 int main() {
 	long length = 0;
-	char* csv_source = read_from_file("tests/test_csv_parse1.csv", &length);
+	char* csv_source = read_from_file("tests/test_csv_parse.csv", &length);
 	printf("%s\n", csv_source);
 	CSValue csv = CSValue_parse(csv_source);
 
-	puts(CSValue_get(&csv, 1, 0));
-	puts(CSValue_get(&csv, 0, 1));
-	puts(CSValue_get(&csv, 0, 0));
 	assert(strcmp(CSValue_get(&csv, 0, 0), "name") == 0);
+	assert(strcmp(CSValue_get(&csv, 1, 0), "species") == 0);
+	assert(strcmp(CSValue_get(&csv, 2, 0), "age") == 0);
+
+	assert(strcmp(CSValue_get(&csv, 0, 1), "Mittens") == 0);
+	assert(strcmp(CSValue_get(&csv, 1, 1), "cat") == 0);
+	assert(strcmp(CSValue_get(&csv, 2, 1), "7") == 0);
+
+	assert(strcmp(CSValue_get(&csv, 0, 2), "Roger") == 0);
+	assert(strcmp(CSValue_get(&csv, 1, 2), "dog") == 0);
+	assert(strcmp(CSValue_get(&csv, 2, 2), "4") == 0);
+
+	assert(strcmp(CSValue_get(&csv, 0, 3), "Bob") == 0);
+	assert(strcmp(CSValue_get(&csv, 1, 3), "human") == 0);
+	assert(strcmp(CSValue_get(&csv, 2, 3), "32") == 0);
 }
 
---
+++ tests/test_csv_parse_unicde.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "../src/parsing.h"
+
+#include "test_utils.h"
+
+int main() {
+	long length = 0;
+	char* csv_source = read_from_file("tests/test_csv_parse_unicode.csv", &length);
+	printf("%s\n", csv_source);
+	CSValue csv = CSValue_parse(csv_source);
+
+	assert(strcmp(CSValue_get(&csv, 0, 0), "emoji") == 0);
+	assert(strcmp(CSValue_get(&csv, 1, 0), "meaning") == 0);
+
+	assert(strcmp(CSValue_get(&csv, 0, 1), "😭") == 0);
+	assert(strcmp(CSValue_get(&csv, 1, 1), "sad human") == 0);
+
+	assert(strcmp(CSValue_get(&csv, 0, 2), "❤️") == 0);
+	assert(strcmp(CSValue_get(&csv, 1, 2), "love is in the air") == 0);
+}
+
